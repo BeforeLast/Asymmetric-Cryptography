4984f836d8672eddb8836031285906b9587e4193
christopherchandrasaputra <13519074@std.stei.itb.ac.id>
2021-10-31 19:54:26 +0700

    Add main program

---
+++ main.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+
+def RSA_GUI():
+    pass
+
+def ElGamal_GUI():
+    pass
+
+def Paillier_GUI():
+    pass
+
+def ECC_GUI():
+    pass
+
+def main():
+    TOOLS = ['RSA','ElGamal','Paillier','ECC']
+    LAYOUT = [
+        [sg.Text('Pick Tools :'),sg.OptionMenu(values=TOOLS,default_value='RSA',key='option'),sg.Button('Pick')]
+    ]
+
+    window = sg.Window('Asymmetric Cryptography Tools', layout=LAYOUT, size=(250,50))
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+            break
+        if event=='Pick':
+            if values['option'] == 'Affine':
+                RSA_GUI()
+            elif values['option'] == 'Playfair':
+                ElGamal_GUI()
+            elif values['option'] == 'Vigenere':
+                Paillier_GUI()
+            elif values['option'] == 'Vigenere Auto-Key':
+                ECC_GUI()
+    window.close()
+
+if __name__ == '__main__':
+    main()
